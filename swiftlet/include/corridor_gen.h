48f14d7af278040f84bd36e7bbacba4eef8bfde8
zhengtian <mazt1025@hotmail.com>
2022-11-17 14:32:06 +0800

    (WIP): corridor gen

--- swiftlet/include/corridor_gen.h
+++ swiftlet/include/corridor_gen.h
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <queue>
+#include <vector>
 #include <algorithm>
 #include <pcl/octree/octree_search.h>
+#include <pcl/point_cloud.h>
 
+using Corridor = std::pair<Eigen::Vector3d, double>;
+using namespace pcl;
 namespace CorridorGen
 {
     class CorridorGenerator
     {
     public:
-        typedef std::pair<Eigen::Vector3d, double> Corridor;
-
     public: // public member variable
         // flight_corridor_[i].first is the position of i^th spherical corridor centre
         // flight_corridor_[i].second is the radius of i^th spherical corridor
@@ -18,31 +20,37 @@ namespace CorridorGen
 
     private: // private member variable
         Eigen::Vector3d initial_position_, goal_position_, local_guide_point_;
+        double resolution;
+        double clearance; // drone radius
 
         // global guide path
-        std::vector<Eigen::Vector3d> input_path_;
+        std::vector<Eigen::Vector3d> guide_path_;
 
         // store the local point cloud
         pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree_ =
             decltype(octree_)(0.1);
 
+        // OctreePointCloudPointVector<PointXYZ> octree_(0.1);
+        // pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree_;
+
     public: // public member function
-        CorridorGenerator();
+        CorridorGenerator(double res);
         ~CorridorGenerator();
 
-        void updatePointCloud();
+        void updatePointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr &new_cloud); // yes
 
         // Generate a sphere SFC given a position
-        auto GenerateOneSphere(const Eigen::Vector3d &pos);
+        Corridor GenerateOneSphere(const Eigen::Vector3d &pos);
 
-        void updateGlobalPath();
+        void updateGlobalPath(const std::vector<Eigen::Vector3d> &path); // yes
 
     private: // private member function
-        Eigen::Vector3d getGuidePoint(const std::vector<Eigen::Vector3d> &input_path_, const Corridor& input_corridor);
-        Corridor batchSample(const Eigen::Vector3d& guide_point, const Corridor& input_corridor);
-        void generateCorridorAlongPath();
+        Eigen::Vector3d getGuidePoint(const std::vector<Eigen::Vector3d> &guide_path, const Corridor &input_corridor);
+        Corridor batchSample(const Eigen::Vector3d &guide_point, const Corridor &input_corridor);
+        void generateCorridorAlongPath(); // yes
+        bool pointInCorridor(const Eigen::Vector3d &point, const Corridor &corridor);
 
-        // more functinos to implement batchSample(...)
+        // more functions to implement batchSample(...)
     };
 
 }; // namespace CorridorGenerator
--- swiftlet/src/corridor_gen.cpp
+++ swiftlet/src/corridor_gen.cpp
@@ -1,3 +1,70 @@
 #include <corridor_gen.h>
 
 using namespace CorridorGen;
+CorridorGenerator::CorridorGenerator(double res)
+    : resolution(res)
+{
+    octree_.deleteTree();
+    octree_.setResolution(resolution);
+
+}
+
+void CorridorGenerator::updatePointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr &new_cloud)
+{
+    octree_.deleteTree();
+    octree_.setInputCloud(new_cloud);
+    octree_.addPointsFromInputCloud();
+}
+
+void CorridorGenerator::updateGlobalPath(const std::vector<Eigen::Vector3d> &path)
+{
+    guide_path_ = path;
+    initial_position_ = path.front(); // front() and back() return read-only reference
+    goal_position_ = path.back();
+
+    generateCorridorAlongPath();
+}
+
+void CorridorGenerator::generateCorridorAlongPath()
+{
+    Corridor current_corridor = GenerateOneSphere(initial_position_);
+    flight_corridor_.emplace_back(current_corridor);
+
+    // todo: implement time check to break from loop
+    while (true)
+    {
+        local_guide_point_ = getGuidePoint(guide_path_, current_corridor);
+        current_corridor = batchSample(local_guide_point_, current_corridor);
+        flight_corridor_.emplace_back(current_corridor);
+
+        if (pointInCorridor(goal_position_, current_corridor))
+        {
+            std::cout << "Corridor Generated" << std::endl;
+            break;
+        }
+    }
+}
+
+Corridor CorridorGenerator::GenerateOneSphere(const Eigen::Vector3d &pos)
+{
+    // lock guard to prevent cloud from changing?
+    int K = 1;
+    std::vector<int> pointIdxNKNSearch;
+    std::vector<float> pointNKNSquaredDistance;
+    pcl::PointXYZ searchPoint;
+    searchPoint.x = pos.x();
+    searchPoint.y = pos.y();
+    searchPoint.z = pos.z();
+
+     // consider using radiusSearch?
+    if (octree_.nearestKSearch(searchPoint, K, pointIdxNKNSearch, pointIdxNKNSearch) > 0)
+    {
+        std::cout << "Found neighbor" << std::endl;
+        return Corridor(pos, sqrt(double(pointNKNSquaredDistance[0])));
+    }
+    else
+    {
+        std::cout << "cloud empty?" << std::endl;
+        return Corridor(pos, 10.0);
+    }
+}
